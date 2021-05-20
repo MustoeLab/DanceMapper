fb3fdf76355b58c28335b95459aa159f46cd8d7f
anthonymustoe <anthony.mustoe@bcm.edu>
2021-05-20 14:48:18 -0500

    updated plotclusters to have compatability with different scipy version

--- plotClusters.py
+++ plotClusters.py
@@ -400,6 +400,9 @@ def printPearson(clust1, clust2):
                 mask = (clust1.rawprofiles[i]>-1) & (clust2.rawprofiles[j]>-1)
      
             corrcoef = stats.pearsonr(clust1.rawprofiles[i][mask], clust2.rawprofiles[j][mask])
+            if isinstance(corrcoef, (list, tuple)):
+                corrcoef = corrcoef[0]
+
             print("{0} {1:.3f}, {2} {3:.3f} : {4:.2f}".format(i,pi,j,pj,corrcoef))
    
 
