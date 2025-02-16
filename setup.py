4aa243999c55ae2c295b978d72d13d5649b5a25b
Patrick Irving <psirving@gmail.com>
2025-02-16 05:08:25 +0000

    chore: add scripts/packages, remove externalpaths

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
-
 #########################################################
 # Code to compile readMutStrings.pyx
 # Anthony Mustoe
 # 2018
 #
-# This file is licensed under the terms of the MIT license  
+# This file is licensed under the terms of the MIT license
 #
 #########################################################
 
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy
+from pathlib import Path
+
+# From RingMapper
+import ringmapper
 
 
-# get path to functions needed for mutstring I/O
-import externalpaths
-
-import sys
-sys.path.append(externalpaths.ringmapper())
-
-ext = Extension('accessoryFunctions', 
-                sources=['accessoryFunctions.pyx'],
-                include_dirs = [numpy.get_include(), externalpaths.ringmapper()])
-
+ringmapper_path = str(Path(ringmapper.__file__).parent)
+ext = Extension(
+    "accessoryFunctions",
+    sources=["accessoryFunctions.pyx"],
+    include_dirs=[numpy.get_include(), ringmapper_path],
+)
 setup(
-    name = "accessoryFunctions",
-    ext_modules = cythonize(ext),
+    name="DanceMapper",
+    ext_modules=cythonize(ext),
+    scripts=["foldClusters.py", "plotClusters.py", "DanceMapper.py"],
+    packages=find_packages(),
 )
